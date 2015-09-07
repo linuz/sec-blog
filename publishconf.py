76dd1f2f2fcf240c65d058ee0df2afca1b4f3565
Dennis Maldonado <dennismald@gmail.com>
2015-09-07 03:04:19 +0000

    Fixing themes folder

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://kernelmeltdown.org/blog'
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
